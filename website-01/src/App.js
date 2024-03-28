3381a0cbaef2923fe99f675b7b204ea94cedf29a
윤효진 <163402670+ttttta95@users.noreply.github.com>
2024-03-29 08:55:49 +0900

    레전드

--- website-01/src/App.js
+++ website-01/src/App.js
@@ -5,17 +5,3 @@ import About from './page/About';
 import Insight from './page/Insight';
 import Lounge from './page/Lounge';
 import Company from './page/Company';
-
-export default function App() {
-    return (
-        <Router>
-            <Navbar />
-            <Routes>
-                <Route path="/about" element={<About />} />
-                <Route path="/insight" element={<Insight />} />
-                <Route path="/lounge" element={<Lounge />} />
-                <Route path="/company" element={<Company />} />
-            </Routes>
-        </Router>
-    );
-}
