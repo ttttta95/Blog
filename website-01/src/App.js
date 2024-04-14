b72ae2ec1b64e08d9732fad240317a86c8973502
윤효진 <163402670+ttttta95@users.noreply.github.com>
2024-04-14 09:00:14 +0900

    와와

--- website-01/src/App.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Navbar from './component/Navbar';
-import About from './page/About';
-import Insight from './page/Insight';
-import Lounge from './page/Lounge';
-import Company from './page/Company';
