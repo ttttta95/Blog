85ef262dbf5cb89c413a3d3c6b02f55ec5d399c6
윤효진 <163402670+ttttta95@users.noreply.github.com>
2024-06-23 19:16:51 +0900

    레전드

--- blog/src/App.js
+++ blog/src/App.js
@@ -1,18 +1,18 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-ruoter-dom';
 import Header from './component/Header';
-import Write from './component/Write';
-import Posts from './component/Posts';
-import Profile from './component/Profile';
+import Write from './page/Write';
+import Posts from './page/Posts';
+import Profile from './page/Profile';
 
 export default function App() {
     return (
         <Router>
           <Header />
           <Routes>
-            <Route path='/write' element={<Write />}></Route>
-            <Route path='/posts' element={<Posts />}></Route>
-            <Route path='/profile' element={<Profile />}></Route>
+            <Route path='/write' element={<Write/>}></Route>
+            <Route path='/posts' element={<Posts/>}></Route>
+            <Route path='/profile' element={<Profile/>}></Route>
           </Routes>
         </Router>
     );
