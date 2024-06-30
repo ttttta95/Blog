550e7e29fc18a2ab1edd70798f5ba9deacda2d6e
윤효진 <163402670+ttttta95@users.noreply.github.com>
2024-06-30 20:06:19 +0900

    커밋

--- blog/src/component/Slide.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-export default function Banner() {
-    return (
-        <div>
-            <h2>배너</h2>
-        </div>
-    );
-}
--- blog/src/page/Home.jsx
+++ blog/src/page/Home.jsx
@@ -1,11 +1,9 @@
 import React from 'react';
-import Banner from '../component/Slide';
 
 export default function Home() {
     return (
         <div>
             <h2>홈</h2>
-            <Banner />
         </div>
     )
 }
