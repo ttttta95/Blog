ccc8a8d4bf2d6f07b1089d11411f750aee5f1a51
윤효진 <163402670+ttttta95@users.noreply.github.com>
2024-07-30 19:59:54 +0900

    커밋

--- blog/src/App.js
+++ blog/src/App.js
@@ -11,7 +11,7 @@ export default function App() {
         <Router>
           <Header />
           <Routes>
-          <Route path="/" element={<Home />} />
+            <Route path="/" element={<Home />} />
             <Route path="/Write" element={<Write />} />
             <Route path="/Posts" element={<Posts />} />
             <Route path="/Profile" element={<Profile />} />
