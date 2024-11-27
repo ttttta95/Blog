0d3f15aad4807ca5e8bdce825a90fbe79f38b083
윤효진 <163402670+ttttta95@users.noreply.github.com>
2024-11-27 22:16:27 +0900

    Header

--- blog/src/component/Header.jsx
+++ blog/src/component/Header.jsx
@@ -1,14 +1,15 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import './Header.css';
 
 export default function Header() {
     return (
-        <header>
-            <h3 className='logo'>React Blog</h3>
-            <nav className='header__menu'>
-                <Link to="/Write">글쓰기</Link>
-                <Link to="/Posts">게시글</Link>
-                <Link to="/Profile">프로필</Link>
+        <header className="header">
+            <h3 className="header__logo">React Blog</h3>
+            <nav className="header__menu">
+                <Link to="/Write" className="header__link">글쓰기</Link>
+                <Link to="/Posts" className="header__link">게시글</Link>
+                <Link to="/Profile" className="header__link">프로필</Link>
             </nav>
         </header>
     );
