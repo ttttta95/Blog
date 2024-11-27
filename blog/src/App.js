5b2b91724606d8c57403c5775233675bca2aa845
윤효진 <163402670+ttttta95@users.noreply.github.com>
2024-11-27 21:45:01 +0900

    Navbar 기본틀 완성

--- blog/src/App.js
+++ blog/src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-ruoter-dom';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Header from './component/Header';
 import Write from './page/Write';
 import Posts from './page/Posts';
