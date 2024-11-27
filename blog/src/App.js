cac6869d0abc2945b5a05239f5ca2d24b1545629
윤효진 <163402670+ttttta95@users.noreply.github.com>
2024-11-27 15:18:47 +0900

    레전드

--- blog/src/App.js
+++ blog/src/App.js
@@ -1,3 +1,6 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-ruoter-dom';
-import
+import Header from './component/Header';
+import Write from './component/Write';
+import Posts from './component/Posts';
+import Profile from './component/Profile';
