27d9fdcbaf8face7ceef55be38848ab9cce10c01
윤효진 <163402670+ttttta95@users.noreply.github.com>
2024-11-28 21:55:54 +0900

    CRUD 인포 작성

--- blog/src/component/Info.jsx
+++ blog/src/component/Info.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 export default function Info() {
     return (
         <div>
-            <p className='info__message'>리액트 연습용 사이트</p>
+            <p className='info__message'>React Practice Site</p>
         </div>
     );
 }
--- blog/src/page/Form.jsx
+++ blog/src/page/Form.jsx
@@ -1,10 +1,9 @@
 import React from 'react';
-import CRUD from '../component/Crud';
 
 export default function Form() {
     return (
         <div>
-            <CRUD />
+            <p className='info__form'>CRUD</p>
         </div>
     );
 }
