78225ebbcc54738cb167e4c2126274a1bc0fc43b
윤효진 <163402670+ttttta95@users.noreply.github.com>
2024-11-29 08:59:46 +0900

    커밋

--- blog/src/component/Info.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-export default function Info() {
-    return (
-        <div>
-            <p className='info__message'>React Practice Site</p>
-        </div>
-    );
-}
